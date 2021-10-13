135e11698b67053bdcbf4d4fe423a8e772a33a81
Fabio <a2020115158@estudiantes.upsa.edu.bo>
2021-10-13 01:19:41 -0400

    Se subió todos las carpetas y archivos

---
+++ WebPeople3/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople3.Models
+{
+    public enum SexType
+    {
+        Female = 0,
+        Male = 1
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage ="El nombre debe contener entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre completo de la persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name = "Pasa tiempo")]
+        public string Hobbie { get; set; }
+
+
+    }
+}
